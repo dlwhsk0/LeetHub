08bbcc1e68b3d1a00f6b79622697c972364b970d
dlwhsk0 <94193594+dlwhsk0@users.noreply.github.com>
2024-03-01 15:30:17 +0900

    Time: 101 ms (70.09%), Space: 17.6 MB (51.45%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m = len(matrix)
+        n = len(matrix[0])
+        pos = []
+        for i in range(m):  # 1. 0의 위치 검사 O(m*n)
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    pos.append([i, j])
+
+        if len(pos) == m*n: return
+
+        for x, y in pos:  # 2. 상하좌우 0으로 만들기 O(0의개수*(m+n))
+            for i in range(m):
+                matrix[i][y] = 0
+            for i in range(n):
+                matrix[x][i] = 0
