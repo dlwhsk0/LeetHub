066b79b9f551161923af0369c0b4bf897b48bdcf
dlwhsk0 <94193594+dlwhsk0@users.noreply.github.com>
2024-03-01 15:31:18 +0900

    Time: 102 ms (65.02%), Space: 17.5 MB (51.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,15 +3,13 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         pos = []
-        for i in range(m):  # 1. 0의 위치 검사 O(m*n)
+        for i in range(m): # 1. 0의 위치 검사 O(m*n)
             for j in range(n):
-                if matrix[i][j] == 0:
-                    pos.append([i, j])
+                if matrix[i][j] == 0: pos.append([i, j])
 
-        if len(pos) == m*n: return
+        for (x, y) in pos: # 2. 상하좌우 0으로 만들기 O(0의개수*(m+n))
+            for i in range(m): matrix[i][y] = 0
+            for i in range(n): matrix[x][i] = 0
 
-        for x, y in pos:  # 2. 상하좌우 0으로 만들기 O(0의개수*(m+n))
-            for i in range(m):
-                matrix[i][y] = 0
-            for i in range(n):
-                matrix[x][i] = 0
+
+        
