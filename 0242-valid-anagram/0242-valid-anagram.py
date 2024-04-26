c302b45c052e3be2422124cefa1b4150ac207b8f
Hana <94193594+dlwhsk0@users.noreply.github.com>
2024-04-26 14:50:05 +0900

    Time: 62 ms (7.94%), Space: 17 MB (64.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(s)
-        t = sorted(t)
-        return s == t
+        if len(s) != len(t): return False
+
+        ds = {}
+        dt = {}
+        l = max(len(s), len(t))
+        for i in range(l):
+            if len(s) > i and s[i] not in ds: ds[s[i]] = 0
+            ds[s[i]] += 1
+            if len(t) > i and t[i] not in dt: dt[t[i]] = 0
+            dt[t[i]] += 1
+        
+        for d in ds:
+            if d not in dt or ds[d] != dt[d]: return False
+        
+        return True
